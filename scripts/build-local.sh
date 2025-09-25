2d148cebcb56b28d515b48fc95336a843b8e9b81
SieR-VR <nwh040512@gmail.com>
2025-09-25 23:56:55 +0900

    chore: indent

--- scripts/build-local.sh
+++ scripts/build-local.sh
@@ -1,1 +1,9 @@
-cd build && make
+#!/bin/bash
+
+[ ! -d pico-sdk ] && \
+ git clone --recursive https://github.com/raspberrypi/pico-sdk
+
+export PICO_SDK_PATH=$(pwd)/pico-sdk
+
+mkdir -p build && cd build && cmake .. -DPICO_SDK_PATH=$PICO_SDK_PATH
+make
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 [ ! -d pico-sdk ] && \
- git clone --recursive https://github.com/raspberrypi/pico-sdk 
+  git clone --recursive https://github.com/raspberrypi/pico-sdk
 
 docker run --rm -it \
- -v $(pwd)/pico-sdk:/pico-sdk \
- -v $(pwd):/project \
- osumaet/build-rp2040 \
- sh -c 'mkdir -p build && cd build && cmake .. && make'
+  -v $(pwd)/pico-sdk:/pico-sdk \
+  -v $(pwd):/project \
+  osumaet/build-rp2040 \
+  sh -c 'mkdir -p build && cd build && cmake .. && make'
